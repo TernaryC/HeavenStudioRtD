c2f712194c82f0739430f85f70987a5fe8661972
Doodles <36998960+TernaryC@users.noreply.github.com>
2024-03-05 17:41:34 -0600

    Update conf.py
    Replaced template information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Heaven Studio RtD'
+author = 'Doodles'
 
 release = '0.1'
 version = '0.1.0'
